318e71ae5c02ed7874ce265f50c4ddb2de1e49b2
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-01-25 21:29:49 -0300

    corregi la url de nuevo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://dragneel98.github.io/rick-and-morty-react/"
+  base: "https://dragneel98.github.io/rick-and-morty-react/"
 })
